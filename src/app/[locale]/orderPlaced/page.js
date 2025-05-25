4bd08c17a0a869d46dbbb0695e292f3c14accbd6
mahmoudmohamedsh <201727100@std.sci.cu.edu.eg>
2025-05-25 11:52:04 +0300

    fix design error

--- src/app/[locale]/orderPlaced/page.js
+++ src/app/[locale]/orderPlaced/page.js
@@ -4,17 +4,34 @@ import dynamic from 'next/dynamic';
 import { Header } from '@/app/[locale]/payment/Header'
 import { OrderDetails } from '@/app/[locale]/orderPlaced/OrderDetails';
 import { Box, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 
 const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
 
 import done from '/src/animation/done.json';
-import {Link} from "@/i18n/navigation"
+import { Link } from "@/i18n/navigation"
 import { useTranslations } from 'next-intl';
 
 const page = () => {
     const t = useTranslations()
+
+    const headerRef = useRef(null);
+    const [headerHeight, setHeaderHeight] = useState(0);
+    useEffect(() => {
+        if (!headerRef.current) return;
+
+        const resizeObserver = new ResizeObserver((entries) => {
+            for (let entry of entries) {
+                setHeaderHeight(entry.contentRect.height);
+            }
+        });
+
+        resizeObserver.observe(headerRef.current);
+
+        // Cleanup on unmount
+        return () => resizeObserver.disconnect();
+    }, []);
     return (
         <Box
             sx={{
@@ -24,11 +41,25 @@ const page = () => {
                 width: '100%',
             }}>
 
-            <Header />
-            <OrderDetails />
+            <Box className="iam here" ref={headerRef}
+                sx={{
+                    position: "fixed",
+                    overflow: 'hidden',
+                    backgroundColor: '#1E1E2A',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    zIndex: 1000,
+                    overflowY: 'hidden'
+                }}>
 
-            <Box display={"flex"} height={"135vh"} justifyContent={"center"}
-                alignItems={"center"} textAlign={"center"} >
+
+                <Header />
+                <OrderDetails />
+            </Box>
+            <Box sx={{ height: `${headerHeight}px` }} />
+            <Box display={"flex"} height={"auto"} justifyContent={"center"}
+                alignItems={"center"} textAlign={"center"}  >
                 <div style={{ width: "250px", height: "250px" }}>
                     <Lottie animationData={done} loop={true}
                         style={{
@@ -38,8 +69,10 @@ const page = () => {
                     <Link href='/tracking' sx={{ textDecoration: "none" }}>
                         <Typography
                             variant="body2"
-                            sx={{ cursor: "pointer", fontSize: "15px", fontWeight: "bold", color: "#AAAAAA", 
-                                textDecoration: "none" }}
+                            sx={{
+                                cursor: "pointer", fontSize: "15px", fontWeight: "bold", color: "#AAAAAA",
+                                textDecoration: "none"
+                            }}
                         >
                             {t("orderPlaced")}
                         </Typography>
